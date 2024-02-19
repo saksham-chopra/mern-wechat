0335f588cca2550bbe82605706c996ea95bf4be2
saksham-chopra <sakshamc03@gmail.com>
2024-02-19 16:15:38 +0530

    Socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5000", {
+			const socket = io("https://wechat-fdsr.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
